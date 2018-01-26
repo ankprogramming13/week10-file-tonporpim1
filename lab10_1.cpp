fc89f7d06981894a149e472cbfec7ca47e58c830
Unknown <pimlapatpat@cvc-cha.ac.th>
2018-01-26 13:19:42 +0700

    ใบงานที่10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 int main() {
+    ofstream myfile ("example.txt");
+    if(myfile.is_open())
+    {
+    myfile << "this is a line.\n";
+    myfile << "this is another line.\n";
+    myfile.close();
+    }
+    else cout <<"Unable to open flie";
         /* code */
         return 0;
 }
